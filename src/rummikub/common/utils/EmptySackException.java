c2fb519430146e4bb089a6f0fa56b24a4868ad66
1hwai <21sunrin128@sunrint.hs.kr>
2022-10-27 10:48:07 +0900

    Update Exceptions

--- src/rummikub/common/utils/EmptySackException.java
+++ src/rummikub/common/utils/EmptySackException.java
@@ -1,5 +1,13 @@
 package rummikub.common.utils;
 
+import rummikub.common.tile.Tile;
+import rummikub.common.tile.TileSack;
+
+/**
+ * The class EmptySackException throws exception
+ * when the {@link TileSack} is empty,
+ * so there's no more {@link Tile} for a player to extract.
+ */
 public class EmptySackException extends Exception{
     public EmptySackException() {
 
